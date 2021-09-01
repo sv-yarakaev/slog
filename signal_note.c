bbd1d3b4151bb27199027164b9791da130b93b48
sv-yarakaev <yar-stas@yandex.ru>
2021-09-01 10:22:38 +0300

    syntax war

--- signal_note.c
+++ signal_note.c
@@ -6,9 +6,6 @@
 
  void on_changed_note(GtkWidget *widget, gpointer note) {
     g_print("Note change\n");
-    //note->add_pages();
-    //GtkNotebook *local  = note;
     GtkWidget *tab2_label;
     tab2_label = gtk_label_new ("first page");
-    //gtk_notebook_insert_page (GTK_NOTEBOOK(note),  GTK_WIDGET(tab2_label), -1);
 }
